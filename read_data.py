5b6ee833ea0d8b32f2a29e15ae3e3870885a0359
Peter Heuz <jheuz@web.de>
2019-06-10 20:09:23 +0200

    tidies up

--- read_data.py
+++ read_data.py
@@ -1,40 +1,33 @@
 from lxml import etree
+import xml.etree.ElementTree as ET
 import pandas as pd
 import pdb
 from datetime import datetime
 
-xml_path = "data_semeval/articles-validation-bypublisher-20181122.xml"
-xsd_path = "data_semeval/article.xsd"
-
+xml_path = "data_semeval/articles-training-bypublisher-20181122.xml"
 # with open(xsd_path) as xsd_file
 # 	xmlschema_doc = etree.parse(f)
 # 	xmlschema = etree.XMLSchema(xmlschema_doc)
 
-from bs4 import BeautifulSoup
+context = ET.parse(xml_path)
+root = context.getroot()
 
-
-# parser = etree.XMLParser(schema=etree.XMLSchema(file=xsd_path))
-# soup = BeautifulSoup(open(xml_path))
-with open(xml_path) as xml_file:
-	# xml_f = xml_file.read()
-	soup = BeautifulSoup(xml_file, "xml")
+df = pd.DataFrame(columns=["id", "title", "published", "text"])
+for child in root :
+	text = "".join(child.itertext())
+	_id = int(child.attrib["id"])
+	try:
+		published = child.attrib["published-at"]
+	except KeyError:
+		published = None
+	title = child.attrib["title"]
+	if _id % 1000 == 0:
+			print(datetime.now().time(), " - " ,
+				str(_id), " articles processed")
+	df.loc[len(df)] = [_id, title, published, text]
 
 pdb.set_trace()
-df = pd.DataFrame(columns=["id", "title", "published", "text"])
-for art in soup.articles:
-	try:
-		text = art.text
-		attribs = art.attrs 
-		if int(attribs["id"]) % 1000 == 0:
-			print(datetime.now().time(), " - " ,
-				str(attribs["id"]), " articles processed")
-		try:
-			df.loc[len(df)] = [attribs["id"], attribs["title"], attribs["published-at"], text]
-		except KeyError:
-			df.loc[len(df)] = [attribs["id"], attribs["title"], None, text]
-	except AttributeError:
-		pass
 
-df.to_csv("data_semeval/articles-validation-bypublisher-20181122.csv")
+df.to_csv("data_semeval/articles-training-bypublisher-20181122.csv")
 	# 	xml_parsed = etree.XML(xml_f, parser)
 
