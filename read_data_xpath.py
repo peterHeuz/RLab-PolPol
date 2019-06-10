c58ae6f611171d3a3c27267265f01aed2832a7b3
Peter Heuz <jheuz@web.de>
2019-06-10 15:03:11 +0200

    adds working data reader also for large files

--- read_data_xpath.py
+++ read_data_xpath.py
@@ -1,69 +1,32 @@
 from lxml import etree
+import xml.etree.ElementTree as ET
 import pandas as pd
 import pdb
 from datetime import datetime
 
-xml_path = "data_semeval/articles-training-byarticle-20181122.xml"
-
+xml_path = "data_semeval/articles-training-bypublisher-20181122.xml"
 # with open(xsd_path) as xsd_file
 # 	xmlschema_doc = etree.parse(f)
 # 	xmlschema = etree.XMLSchema(xmlschema_doc)
 
-context = etree.iterparse(xml_path, tag='article' )
+context = ET.parse(xml_path)
+root = context.getroot()
 
 df = pd.DataFrame(columns=["id", "title", "published", "text"])
-for event, elem in context :
-	text = elem.text
-	if text == None:
-		pdb.set_trace()
-	if len(elem.xpath("@title")) == 1:
-		title = elem.xpath("@title")[0] 
-	else:
-		title = None
-	if len(elem.xpath("@published-at")) == 1:
-		published = elem.xpath("@published-at")[0] 
-	else:
-		published = None 
-	if len(elem.xpath("@id")) == 1:
-		_id = int(elem.xpath("@id")[0])
-	else:
-		_id = None
+for child in root :
+	text = "".join(child.itertext())
+	_id = int(child.attrib["id"])
+	# TODO convert to datetime
+	try:
+		published = child.attrib["published-at"]
+	except KeyError:
+		published = None
+	title = child.attrib["title"]
 	if _id % 1000 == 0:
 			print(datetime.now().time(), " - " ,
 				str(_id), " articles processed")
 	df.loc[len(df)] = [_id, title, published, text]
 
-pdb.set_trace()
-
-del context
-
-
-
-from bs4 import BeautifulSoup
-
-
-# parser = etree.XMLParser(schema=etree.XMLSchema(file=xsd_path))
-# soup = BeautifulSoup(open(xml_path))
-with open(xml_path) as xml_file:
-	# xml_f = xml_file.read()
-	soup = BeautifulSoup(xml_file, "xml")
-
-pdb.set_trace()
-df = pd.DataFrame(columns=["id", "title", "published", "text"])
-for art in soup.articles:
-	try:
-		text = art.text
-		attribs = art.attrs 
-		if int(attribs["id"]) % 1000 == 0:
-			print(datetime.now().time(), " - " ,
-				str(attribs["id"]), " articles processed")
-		try:
-			df.loc[len(df)] = [attribs["id"], attribs["title"], attribs["published-at"], text]
-		except KeyError:
-			df.loc[len(df)] = [attribs["id"], attribs["title"], None, text]
-	except AttributeError:
-		pass
-
-df.to_csv("data_semeval/articles-validation-bypublisher-20181122.csv")
+df.to_csv("data_semeval/articles-training-bypublisher-20181122.csv")
 	# 	xml_parsed = etree.XML(xml_f, parser)
 
