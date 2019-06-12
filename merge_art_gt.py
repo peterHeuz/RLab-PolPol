7c8dcbf3e40dfd213abc2aed304fff5299f1b6e3
Peter Heuz <jheuz@web.de>
2019-06-12 14:07:26 +0200

    fixed minor bug of 'Unnamed: 0' column

--- merge_art_gt.py
+++ merge_art_gt.py
@@ -3,11 +3,11 @@ import sys
 
 def main(art_path, gt_path):
 	art = pd.read_csv("data_semeval/" + art_path)
-	art.drop("Unnamed: 0", axis=1)
+	art = art.drop("Unnamed: 0", axis=1)
 	gt = pd.read_csv("data_semeval/" + gt_path)
-	gt.drop("Unnamed: 0", axis=1)
+	gt = gt.drop("Unnamed: 0", axis=1)
 	df = art.merge(gt,on="id")
-	df.to_csv("data_semeval/articles-" + gt_path)
+	df.to_csv("data_semeval/articles-" + gt_path, index=False)
 
 if __name__ == '__main__':
 	main(sys.argv[1], sys.argv[2])
