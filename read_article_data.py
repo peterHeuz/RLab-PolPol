00cfe1890c9d99388e933355909ce49b4ef26fb4
Peter Heuz <jheuz@web.de>
2019-07-07 14:56:58 +0200

    adds iterparse to save memory

--- read_article_data.py
+++ read_article_data.py
@@ -3,25 +3,34 @@ import pandas as pd
 from datetime import datetime
 import sys 
 
+import pdb
+
 def main(file_path):
 	xml_path = "data_semeval/" + file_path + ".xml"
 
-	context = ET.parse(xml_path)
-	root = context.getroot()
+	context = ET.iterparse(xml_path, events=("start", "end"))
+	# turn it into an iterator
+	context = iter(context)
+
+	# get the root element
+	event, root = context.__next__()
+
 
 	df = pd.DataFrame(columns=["id", "title", "published", "text"])
-	for child in root :
-		text = "".join(child.itertext())
-		_id = int(child.attrib["id"])
-		try:
-			published = child.attrib["published-at"]
-		except KeyError:
-			published = None
-		title = child.attrib["title"]
-		if _id % 1000 == 0:
-				print(datetime.now().time(), " - " ,
-					str(_id), " articles processed")
-		df.loc[len(df)] = [_id, title, published, text]
+	for event, elem in context:
+		if event == "end" and elem.tag == "article":
+			text = "".join(elem.itertext())
+			_id = int(elem.attrib["id"])
+			try:
+				published = elem.attrib["published-at"]
+			except KeyError:
+				published = None
+			title = elem.attrib["title"]
+			if _id % 1000 == 0:
+					print(datetime.now().time(), " - " ,
+						str(_id), " articles processed")
+			df.loc[len(df)] = [_id, title, published, text]
+			root.clear()
 
 	# converts published column to timestamps
 	df["published"] = df["published"].apply(lambda x: datetime.strptime(x, '%Y-%m-%d') if not pd.isnull(x) else None)
